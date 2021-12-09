5ad6981f6b04eb69d9750f23074efe089bbcefea
Sali_dk52@itstepru.onmicrosoft.com <Sali_dk52@itstepru.onmicrosoft.com>
2021-12-09 21:34:46 +0300

    classwork

--- DataContainers/main.cpp
+++ DataContainers/main.cpp
@@ -9,17 +9,21 @@ class Element
 {
 	int Data; // Значение элемента
 	Element* pNext; // адрес следующего элемента
+	static int count; // КОЛИЧЕСТВО ЭЛЕМЕНТОВ
 public:
 	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
 	{
+		count++;
 		cout << "Econstructor:\t" << this << endl;
 	}
 	~Element()
 	{
+		count--;
 		cout << "Edustructor:\t" << this << endl;
 	}
 	friend class ForwardList;
 };
+int Element::count = 0; // Статические переменные могут быть проинициализированы только за классом.
 class ForwardList
 {
 	Element* Head; // Точка входа, указывает на начальный элемент списка.
@@ -43,18 +47,46 @@ public:
 		// 3) Переносим голову на новый элемент
 		Head = New;
 	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr)return	push_front(Data);
+		Element* Temp = Head;
+		while (Temp->pNext)
+			Temp = Temp->pNext;
+		Temp->pNext = new Element(Data);
+
+	}
 	void pop_front()
 	{
-		Head = Head->pNext;
+		Element* erased = Head;
+		 Head = Head->pNext;
+		 delete erased;
 	}
 	void pop_back()
 	{
+		if (Head->pNext == nullptr) return pop_front();
 		Element* Temp = Head;
-		while (Temp->pNext->pNext != NULL)
+		while (Temp->pNext->pNext)Temp = Temp->pNext;
+		delete Temp->pNext;
+		Temp->pNext = nullptr;
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index > Head->count)
 		{
-			Temp = Temp->pNext;
+			cout << "Error: Выход за пределы списка, попробуйте соблюдать границы" << endl;
+			return;
 		}
-		Temp->pNext = NULL;
+		if (Index == 0 || Head == nullptr) return push_front(Data);
+	    //1) Создаём новый элемент
+		Element* New = new Element(Data);
+		//2) Доходим до нужного элемента
+		Element* Temp = Head;
+		for (int i = 0; i < Index-1; i++) Temp = Temp->pNext;
+		// Осуществляем вставку нового элемента в список
+		//3 Привязываем
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
 	}
 	// Methods:
 	void print()const
@@ -66,6 +98,7 @@ public:
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext; // Переход на следующий элемент
 		}
+		cout << "Количество элементов списка: " << Head->count << endl;
 	}
 };
 void main()
@@ -76,7 +109,7 @@ void main()
 	ForwardList list;
 	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		list.push_back(rand() % 100);	
 	}
 	list.print();
 	cout << endl;
@@ -85,5 +118,14 @@ void main()
 	cout << endl;
 	list.pop_back();
 	list.print();
+	cout << endl; 
+	list.push_back(123);
+	list.print();
 	cout << endl;
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента:\n"; cin >> index;
+	cout << "Введите Значение добавляемого элемента:\n"; cin >> value;
+	list.insert(value, index);
+	list.print();
 }
